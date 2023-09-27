d6e3ac85b8722c8d407621bf2c1c7491cd5615f9
Elmar1k <permsalimanov2020@gmail.com>
2023-09-27 11:30:30 +0500

    new Pr 6
    new

--- Pr 6/otv/2otvet.php
+++ Pr 6/otv/2otvet.php
@@ -2,10 +2,15 @@
 <body>
  
 <?php
-if( $_SERVER['REQUEST_METHOD'] === 'POST' ){
+
+if( $_SERVER['REQUEST_METHOD'] !== 'POST' )
+    exit('Не POST');
+
+if(empty($_POST['twoName']) || empty($_POST['twoName'])) exit('Не все поля заполнены');
+if(!is_numeric($_POST['twoName']) || $_POST['twoName'] < 1) exit('Поле возраст заполнено неправильно');
 echo 'Здравствуйте, '.htmlspecialchars($_POST['firstName']).'. <br>';
 echo 'Вам '.(int)$_POST['twoName'].' лет.'.'<br>';
-}
+
 ?>
 </body>
 </html>
